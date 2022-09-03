12a7a8eed21512f9355923f384dec4810c79a365
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-04 03:08:44 +0500

    Add functionality to validate input and check if email is unique

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&directConnection=true&tls=false";
+const mongoURI = "mongodb://localhost:27017/mynotebook?readPreference=primary&directConnection=true&tls=false";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
