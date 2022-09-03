c9fb6d88ee977bc8aae490b7e4ddcc26249982ea
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-03 21:19:12 +0500

    Storing data into database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  obj = {
-    name: 'Auth'
-  };
-
-  res.json(obj);
+// Create a user using POST: '/api/auth'. Does not require authentication
+router.post('/', (req, res) => {
+  
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 
 });
 
