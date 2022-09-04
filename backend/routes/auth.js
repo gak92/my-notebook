69a02e0db7d436993c1ec61c9463f7ee9c1b94ac
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-04 13:46:38 +0500

    Implement password hasing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 // Create a user using: POST '/api/auth/createuser'. No Login required
@@ -25,11 +26,16 @@ async (req, res) => {
     if (user) {
       return res.status(400).json({error: 'Sorry a user already exist with this email'});
     }
-    
+
+    // create secure password using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password, salt);
+
+    // create new user    
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPassword,
     });
     
     res.json(user);
