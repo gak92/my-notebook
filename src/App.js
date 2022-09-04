0b3c866fc022eca709b3dd12d773380dd6a53c66
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-04 20:57:03 +0500

    Add Navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Notebook</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
