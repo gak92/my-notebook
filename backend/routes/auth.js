5f1d0c45bf6fdb6d1d2fb54a8cbf45840bc1b226
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-04 14:21:48 +0500

    Add functionality for login user endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -54,9 +54,53 @@ async (req, res) => {
   }
   catch(e) {
     console.log(e.message);
-    res.status(500).send('Some error occured');
+    res.status(500).send('Internal Server Error');
   }
   
 });
 
+// Authenticate a user: POST "/api/auth/login" - No Login required
+router.post('/login',
+  [
+    body('email', 'Enter valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+  ],
+
+  async (req, res) => {
+    // If errors, return Bad Request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+
+    try {
+      let user = await User.findOne({email});
+      if(!user) {
+        return res.status(400).json({error: "Please enter correct credentials"});
+      }
+
+      const passwordCompared = await bcrypt.compare(password, user.password);
+      if(!passwordCompared) {
+        return res.status(400).json({error: "Please enter correct credentials"});
+      }
+
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+  
+      const authToken = jwt.sign(data, JWT_SECRET);      
+      res.json({authToken});
+
+    }
+    catch(e) {
+      console.log(e.message);
+      res.status(500).send('Internal Server Error');
+    }
+
+  }
+);
 module.exports = router;
