3faec58da59fa50a21ee359410369608ab344880
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-05 12:56:45 +0500

    Add NoteItem components

---
+++ src/components/Notes.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import noteContext from '../context/notes/NoteContext';
+import NoteItem from './NoteItem';
+
+const Notes = () => {
+  const context = useContext(noteContext);
+  const { notes, setNotes } = context;
+
+  return (
+    <div>
+      <h1>Your Notes</h1>
+        {notes.map((note) => {
+          return (
+            <NoteItem note={note}/>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Notes;
